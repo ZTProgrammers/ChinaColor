0548c5e543744a965302ea1dd3f85c90bd292e22
VleaStwo <78075553+VleaStwo@users.noreply.github.com>
2025-04-30 09:44:38 +0800

    chore: configure github pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ChinaColor/", // 添加这一行，使用仓库名作为 base
   plugins: [react()],
   resolve: {
     alias: {
